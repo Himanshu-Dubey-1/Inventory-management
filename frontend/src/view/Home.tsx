70046575a0fb584ca5512d33b3b694892ac44cff
Himanshu-Dubey-1 <Himanshudubey241@gmail.com>
2025-05-05 16:48:56 +0530

    ....

--- frontend/src/view/Home.tsx
+++ frontend/src/view/Home.tsx
@@ -70,13 +70,16 @@ export default function Home() {
               </dl>
             </div>
           </div>
+          <div
+          className="pr-4">
           <img
             alt="Product screenshot"
             src="https://tailwindui.com/plus-assets/img/component-images/dark-project-app-screenshot.png"
             width={2432}
             height={1442}
-            className="w-[48rem] max-w-none rounded-xl ring-1 shadow-xl ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0 mr-5"
+            className="w-[48rem] max-w-none rounded-xl ring-1 shadow-xl ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
           />
+          </div>
         </div>
       </div>
     </div>
